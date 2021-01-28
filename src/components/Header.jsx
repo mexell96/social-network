06d5b8f24682f98f2a7cfc6b36c7d4636b144a05
mexell96 <mexell96@gmail.com>
2021-01-28 11:11:39 +0300

    lesson-12

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://secure.meetupstatic.com/photos/event/7/3/9/d/highres_469109597.jpeg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://venngage-wordpress.s3.amazonaws.com/uploads/2018/09/Colorful-Geometric-Simple-Background-Image.jpg"
+          alt=""
+        />{" "}
+        Main Content
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>
+          New post
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
