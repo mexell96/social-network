9758c8b53747f0e0f2cc0988f2077d271151b744
mexell96 <mexell96@gmail.com>
2021-01-28 12:06:38 +0300

    lesson-14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import classes from "./Header.module.css";
+
 const Header = () => {
   return (
-    <header className="header">
+    <header className={classes.header}>
       <img
         src="https://secure.meetupstatic.com/photos/event/7/3/9/d/highres_469109597.jpeg"
         alt="logo"
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,22 @@
+import classes from "./Profile.module.css";
+
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={classes.content}>
       <div>
         <img
           src="https://venngage-wordpress.s3.amazonaws.com/uploads/2018/09/Colorful-Geometric-Simple-Background-Image.jpg"
           alt=""
-        />{" "}
+        />
         Main Content
       </div>
       <div>ava + description</div>
       <div>
         My posts
-        <div>
+        <div className="posts">
           New post
-          <div>post 1</div>
-          <div>post 2</div>
+          <div className={classes.item}>post 1</div>
+          <div className={classes.item}>post 2</div>
         </div>
       </div>
     </div>
