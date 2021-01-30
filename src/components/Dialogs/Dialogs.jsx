9157b6924620ff8c4af302b624b9e9920c0c33e7
mexell96 <mexell96@gmail.com>
2021-01-30 22:43:52 +0300

    lesson-31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,35 @@
 import style from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
+import React from "react";
 
 const Dialogs = (props) => {
   let dialogsElements = props.state.dialogs.map((d) => (
     <DialogItem name={d.name} id={d.id} />
   ));
   let messagesElements = props.state.messages.map((m) => (
-    <Message message={m.message} id={m.id}/>
+    <Message message={m.message} id={m.id} />
   ));
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
 
   return (
     <div className={style.dialogs}>
       <div className={style.dialogsItems}>{dialogsElements}</div>
-      <div className={style.messages}>{messagesElements}</div>
+      <div className={style.messages}>
+        {messagesElements}
+        <div>
+          <textarea ref={newPostElement}></textarea>
+        </div>
+        <div>
+          <button onClick={addPost}>Add post</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import style from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
@@ -6,15 +7,22 @@ const MyPost = (props) => {
     <Post message={p.message} likesCount={p.likesCount} />
   ));
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
+
   return (
     <div className={style.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={style.posts}>{postsElements}</div>
