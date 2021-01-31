08bed92afeaadf78723d2b514f546e9e889e2e1e
mexell96 <mexell96@gmail.com>
2021-01-31 09:56:38 +0300

    lesson-33

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,6 +12,7 @@ const MyPost = (props) => {
   let addPost = () => {
     let text = newPostElement.current.value;
     props.addPost(text);
+    newPostElement.current.value = "";
   };
 
   return (
