5e387da3bfcc05ec46d34272e7ec398b4eeccc57
mexell96 <mexell96@gmail.com>
2021-02-07 20:09:12 +0300

    lesson-61

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route } from "react-router-dom";
 import "./App.css";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Header from "./components/Header/Header";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
@@ -13,7 +13,7 @@ const App = (props) => {
   console.log(props, "APP");
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         <Route path="/dialogs" render={() => <DialogsContainer />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
+import { NavLink } from "react-router-dom";
 import style from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={style.header}>
       <img
         src="https://secure.meetupstatic.com/photos/event/7/3/9/d/highres_469109597.jpeg"
         alt="logo"
       />
+      <div className={style.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
     </header>
   );
 };
